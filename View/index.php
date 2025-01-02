c68bdef40939c7a3ad7a5b5f1450e2478f346e92
laamiri-kaoutar <kaoutarlaamiri355@gmail.com>
2025-01-02 13:00:38 +0100

    feat(#10): Display reviews dynamically for the user

--- View/index.php
+++ View/index.php
@@ -145,7 +145,6 @@ $aviss = $avis->readByUser($user);
         </div>
         <!-- Spinner End -->
 
-
         <!-- Navbar & Hero Start -->
         <div class="container-fluid nav-bar p-0">
             <nav class="navbar navbar-expand-lg navbar-light bg-white px-4 px-lg-5 py-3 py-lg-0">
@@ -345,10 +344,6 @@ $aviss = $avis->readByUser($user);
             </div>
         </div>
         <!-- Features End -->
-
-
-
-
 
         <!-- Testimonial Start -->
         <div class="container-fluid testimonial overflow-hidden pb-5">
@@ -580,126 +575,6 @@ $aviss = $avis->readByUser($user);
 </div>
 
 
-
- 
-
-
-        <!-- Training Start -->
-        <!-- <div class="container-fluid training overflow-hidden bg-light py-5">
-            <div class="container py-5">
-                <div class="section-title text-center mb-5 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="sub-style">
-                        <h5 class="sub-title text-primary px-3">My Reservations</h5>
-                    </div>
-                    <h1 class="display-5 mb-4">View and Manage Your Bookings</h1>
-                    <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat deleniti amet at atque sequi quibusdam cumque itaque repudiandae temporibus, eius nam mollitia voluptas maxime veniam necessitatibus saepe in ab? Repellat!</p>
-                </div>
-                <div class="row g-4">
-                <div class="col-lg-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="training-item">
-                            <div class="training-inner">
-                                <img src="img/training-1.jpg" class="img-fluid w-100 rounded" alt="Image">
-                                <div class="training-title-name">
-                                    <a href="#" class="h4 text-white mb-0">IELTS</a>
-                                    <a href="#" class="h4 text-white mb-0">Coaching</a>
-                                </div>
-                            </div>
-                            <div class="training-content bg-secondary rounded-bottom p-4">
-                                <a href="#"><h4 class="text-white">IELTS Coaching</h4></a>
-                                <p class="text-white-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, veritatis.</p>
-                                <a class="btn btn-secondary rounded-pill text-white p-0" href="#">Read More <i class="fa fa-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.3s">
-                        <div class="training-item">
-                            <div class="training-inner">
-                                <img src="img/training-2.jpg" class="img-fluid w-100 rounded" alt="Image">
-                                <div class="training-title-name">
-                                    <a href="#" class="h4 text-white mb-0">TOEFL</a>
-                                    <a href="#" class="h4 text-white mb-0">Coaching</a>
-                                </div>
-                            </div>
-                            <div class="training-content bg-secondary rounded-bottom p-4">
-                                <a href="#"><h4 class="text-white">TOEFL Coaching</h4></a>
-                                <p class="text-white-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, veritatis.</p>
-                                <a class="btn btn-secondary rounded-pill text-white p-0" href="#">Read More <i class="fa fa-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="training-item">
-                            <div class="training-inner">
-                                <img src="img/car-rent-1.png" class="img-fluid w-100 rounded" alt="Image">
-                                <div class="training-title-name">
-                                    <a href="#" class="h4 text-white mb-0">IELTS</a>
-                                </div>
-                            </div>
-                            <div class="training-content bg-secondary rounded-bottom p-4">
-                                <a href="#"><h5 class="text-white">IELTS Coaching</h5></a>
-                                <p class="text-white-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, veritatis.</p>
-                                <a class="btn btn-secondary rounded-pill text-white p-0" href="#">Read More <i class="fa fa-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.3s">
-                        <div class="training-item">
-                            <div class="training-inner">
-                                <img src="img/car-rent-2.png" class="img-fluid w-100 rounded" alt="Image">
-                                <div class="training-title-name">
-                                    <a href="#" class="h4 text-white mb-0">TOEFL</a>
-                                    <a href="#" class="h4 text-white mb-0">Coaching</a>
-                                </div>
-                            </div>
-                            <div class="training-content bg-secondary rounded-bottom p-4">
-                                <a href="#"><h4 class="text-white">TOEFL Coaching</h4></a>
-                                <p class="text-white-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, veritatis.</p>
-                                <a class="btn btn-secondary rounded-pill text-white p-0" href="#">Read More <i class="fa fa-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.5s">
-                        <div class="training-item">
-                            <div class="training-inner">
-                                <img src="img/car-rent-3.png" class="img-fluid w-100 rounded" alt="Image">
-                                <div class="training-title-name">
-                                    <a href="#" class="h4 text-white mb-0">PTE</a>
-                                    <a href="#" class="h4 text-white mb-0">Coaching</a>
-                                </div>
-                            </div>
-                            <div class="training-content bg-secondary rounded-bottom p-4">
-                                <a href="#"><h4 class="text-white">PTE Coaching</h4></a>
-                                <p class="text-white-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, veritatis.</p>
-                                <a class="btn btn-secondary rounded-pill text-white p-0" href="#">Read More <i class="fa fa-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.7s">
-                        <div class="training-item">
-                            <div class="training-inner">
-                                <img src="img/car-rent-4.png" class="img-fluid w-100 rounded" alt="Image">
-                                <div class="training-title-name">
-                                    <a href="#" class="h4 text-white mb-0">OET</a>
-                                    <a href="#" class="h4 text-white mb-0">Coaching</a>
-                                </div>
-                            </div>
-                            <div class="training-content bg-secondary rounded-bottom p-4">
-                                <a href="#"><h4 class="text-white">OET Coaching</h4></a>
-                                <p class="text-white-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, veritatis.</p>
-                                <a class="btn btn-secondary rounded-pill text-white p-0" href="#">Read More <i class="fa fa-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 text-center">
-                        <a class="btn btn-primary border-secondary rounded-pill py-3 px-5 wow fadeInUp" data-wow-delay="0.1s" href="#">View More</a>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-        <!-- Training End -->
-
-
-    
 
 
         <!-- Footer Start -->
